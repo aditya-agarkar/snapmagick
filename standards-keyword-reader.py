2e50a6b9e7bb059d7f87bc7737c3645af5ff59a7
aditya-agarkar <adityaagarkar@adityas-MacBook-Pro.local>
2015-06-19 21:30:22 -0700

    Added csv files. Added keyword fixer

---
+++ standards-keyword-reader.py
@@ -0,0 +1,56 @@
+__author__ = 'SantoshPappachan'
+
+import csv
+
+final = []
+with open("MetaData/kw_standards_new.csv","rb") as s:
+    rstandards = csv.reader(s)
+    wordlist = list(rstandards)
+
+with open("MetaData/final_keyword.csv","rb") as k:
+    keywords = csv.reader(k)
+    keywordlist = list(keywords)
+
+
+for word in wordlist:
+    current = word[5].split()
+    exceptFile = open("exceptions.txt","wb")
+    isThere = False
+    remove = False
+    replace = False
+    replacement = ""
+    for keyword in current:
+        for frekey in keywordlist:
+            if keyword == frekey[0]:
+                isThere = True
+                if frekey[2] != "":
+                    replace = True
+                    replacement = frekey[2]
+                elif frekey[3] == "y":
+                    remove = True
+        if isThere == False:
+            exceptFile.write(keyword + "\n")
+
+        if remove == True:
+            current[current.index(keyword)] = ""
+        elif replace == True:
+            current[current.index(keyword)] = replacement
+
+        isThere = False
+        remove = False
+        replace = False
+    final.append(' '.join(current))
+
+
+
+
+with open("MetaData/final_kw_standards.csv","wb") as s:
+    wstandards = csv.writer(s)
+    i = 1
+
+    while i < len(final):
+        wordlist[i][6] = final[i]
+        wstandards.writerow(wordlist[i])
+
+        i += 1
+
