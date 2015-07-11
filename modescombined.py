6715fe978cb8c090928c3cf3a7f873084232c510
aditya-agarkar <adityaagarkar@adityas-MacBook-Pro.local>
2015-07-11 11:54:40 -0700

    more objects and kw

--- modescombined.py
+++ modescombined.py
@@ -410,7 +410,7 @@ with open(standardsFileName,"rU") as f:
                       #rand_col = rand_lighter_color(bg)
                       if( length < 60):
                             commFile.write("<span font=\"Montserrat Bold\" size=\"15000\"")
-			                textColor=fglist[random.randint(0,len(fglist)-1)]
+                            textColor=fglist[random.randint(0,len(fglist)-1)]
                             commFile.write(' foreground="'+textColor+'">' + w.upper() + ' </span>')
                             pos += 1
 
